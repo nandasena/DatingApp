94504898ac94ede70b48210571092f9eedb395c7
phsnandasena <phsnandasena060@gmail.com>
2018-06-23 22:08:23 +0530

    secon commit

---
+++ DatingApp.SPA/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ValueComponent
+],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
