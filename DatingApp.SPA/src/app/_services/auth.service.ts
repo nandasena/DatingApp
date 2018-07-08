9645275d398282e6ab4bfdd85c15ebecf669af45
phsnandasena <phsnandasena060@gmail.com>
2018-07-08 22:48:50 +0530

    error habble

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
@@ -18,14 +21,34 @@ constructor( private http: Http) { }
                 localStorage.setItem('token', user.tokenString);
                 this.userToken = user.tokenString;
             }
-        });
+        }).catch(this.handleError);
     }
 
     register(model: any) {
-        return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+        return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
     }
     private requestOptions() {
         const headers = new Headers({'Content-type': 'application/json'});
         return new RequestOptions({ headers: headers });
+    }
+
+    private handleError(error: any) {
+        const appliicationError = error.headers.get('Application-Error');
+        if (appliicationError) {
+            return Observable.throw(appliicationError);
+        }
+        const serverError = error.json();
+        let modelStateErrors = '';
+        if (serverError) {
+            for (const key in serverError) {
+                if (serverError[key]) {
+                    modelStateErrors += serverError[key] + '\n';
+                }
+            }
+        }
+        return Observable.throw(
+            modelStateErrors || 'Server error'
+        );
+
     }
 }
